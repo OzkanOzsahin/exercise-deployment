482a3aab0b2a88858cdb52b08f799878e4166bd7
Ozkan Ozsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2023-06-25 13:16:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-message = "Dit is mijn opdracht: deployment"
-print(message)
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return 'Hello, welcome to my exercise deployment'
+
+@app.route('/cow')
+def cow():
+    return 'MOoooOo!'
+
+
+if __name__ == '__main__':
+    
+   app.run(port=8080)
+
+
+    
+   
+
